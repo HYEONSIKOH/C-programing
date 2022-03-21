3bb2e54faaa8647343e72c7732d3509572e454eb
HYEONSIK OH <ks06891@naver.com>
2022-03-21 17:05:41 +0900

    중복될 수 있는 파일명 수정

--- 0315 실습.c
+++ 0315 실습.c
@@ -1,4 +1,4 @@
-#define _CRT_SECURE_NO_WARNINGS
+/*#define _CRT_SECURE_NO_WARNINGS
 #include <stdio.h> 
 #include <stdlib.h>
 #include <string.h>
@@ -189,3 +189,4 @@ int main(void) {
 		}
 	}
 }
+*/
