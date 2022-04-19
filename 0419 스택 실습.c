c34a012c85500db746f069e0232e300ca9413b81
HYEONSIK OH <ks06891@naver.com>
2022-04-19 14:24:37 +0900

    0419 데이터구조 실습 및 과제

---
+++ 0419 스택 실습.c
@@ -0,0 +1,163 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <windows.h>
+
+#define MAZE_SIZE 10
+#define MAX 100
+
+typedef struct {
+	short r;
+	short c;
+}element;
+
+element here = { 1,0 }; // 현 위치
+
+char maze[MAZE_SIZE][MAZE_SIZE] = { // 미로 e가 현재 위치, 0은 경로, 1은 벽, x는 출구
+{ '1', '1', '1', '1', '1', '1', '1', '1', '1', '1'},
+{ 'e', '0', '0', '0', '0', '0', '0', '0', '0', '0'},
+{ '0', '1', '0', '1', '1', '1', '1', '1', '1', '0'},
+{ '1', '1', '0', '1', '1', '1', '1', '1', '1', '0'},
+{ '1', '1', '0', '0', '0', '0', '1', '1', '1', '0'},
+{ '1', '1', '1', '1', '1', '0', '1', '1', '1', '0'},
+{ '1', '1', '1', '1', '1', '0', '1', '1', '1', '0'},
+{ '1', '1', '1', '1', '1', '0', '1', '1', '1', '0'},
+{ '1', '1', '1', '1', '1', '0', '0', '0', '0', 'x'},
+{ '1', '1', '1', '1', '1', '1', '1', '1', '1', '1'},
+};
+
+typedef struct {
+	element data[MAX];
+	int top;
+}stacktype;
+
+int is_full(stacktype* s) { // 꽉 찼을때
+	return (s->top == 99);
+}
+
+int is_empty(stacktype* s) {// 텅텅 비었을때
+	return (s->top == -1);
+}
+
+void init_stack(stacktype* s) {// 스택 초기화
+	s->top = -1;
+}
+
+void push(stacktype* s, element c) { // 입력함수
+	if (is_full(s)) {
+		printf("FULL");
+		exit(1);
+	}
+	else
+		s->data[++s->top] = c;
+}
+
+element pop(stacktype* s) { // 삭제 함수
+	if (is_empty(s)) {
+		printf("EMPTY");
+		exit(1);
+	}
+	else
+		return s->data[(s->top)--];
+}
+
+element peak(stacktype* s) { // 참조 함수
+	if (is_empty(s)) {
+		printf("3오류입니다.");
+		exit(1);
+	}
+	else
+		return s->data[s->top];
+}
+
+void peak_print(stacktype* s) { // 참조 함수
+	if (is_empty(s)) {
+		printf("3오류입니다.");
+		exit(1);
+	}
+	else
+		printf("[%d , %d]\n", s->data[s->top].r, s->data[s->top].c);
+}
+
+void push_loc(stacktype* s, int r, int c) { // 현 위치를 화면에 출력한다.
+	if (r < 0 || c < 0) return;
+	else if (r > 9 || c > 9) return;
+	else if (maze[r][c] != '1' && maze[r][c] != '.') {
+		element tmp;
+		tmp.r = r;
+		tmp.c = c;
+		push(s, tmp);
+	}
+	else
+		return;
+}
+
+void maze_print(char maze[MAZE_SIZE][MAZE_SIZE]) { // 미로 화면 출력
+	printf("\n");
+	for (int r = 0; r < MAZE_SIZE; r++) {
+		for (int c = 0; c < MAZE_SIZE; c++) {
+			printf("%c", maze[r][c]);
+		}
+		printf("\n");
+	}
+}
+
+int main(void) {
+	int num;
+	stacktype m;
+	init_stack(&m);
+	int r, c;
+
+	printf("0. 종료\n1. 입구부터 출구까지의 미로경로를 출력(한가지만)\n2. 모든 탈출경로 출력\n\n");
+	printf("숫자를 입력해주세요: ");
+	scanf("%d", &num);
+	printf("-------------------------------------------------------\n");
+	switch (num) {
+	case 0: printf("시스템을 종료합니다.......\n"); Sleep(2000); exit(1); // 0이면 종료
+	case 1:
+		while (1) {
+			r = here.r;
+			c = here.c;
+			if (maze[r][c] == 'x') {
+				printf("\n****** 성공 ******\n\n");
+				exit(1);
+			}
+			else {
+				maze[r][c] = '.';
+				push_loc(&m, r + 1, c); //상
+				push_loc(&m, r - 1, c); //하
+				push_loc(&m, r, c - 1); //좌
+				push_loc(&m, r, c + 1); //우
+				here = pop(&m);
+				maze_print(maze);
+			}
+			
+		}
+	case 2:
+		while (1) {
+			r = here.r;
+			c = here.c;
+			if (maze[r][c] == 'x') {
+				printf("\n****** 성공 ******\n\n");
+				here = pop(&m);
+			}
+			else {
+				maze[r][c] = '.';
+				push_loc(&m, r + 1, c); //상
+				push_loc(&m, r - 1, c); //하
+				push_loc(&m, r, c - 1); //좌
+				push_loc(&m, r, c + 1); //우
+
+				maze_print(maze);
+				if (is_empty(&m)) { // 스택이 텅텅 비어서 더이상 길이 없을 때
+					printf("\n****** 실패! ******\n\n");
+					break;
+				}
+
+				else // 다음 칸으로 넘어갈 때
+					here = pop(&m);
+			}
+		}
+	}
+	return 0;
+}
---
+++ 0419 큐 실습.c
@@ -0,0 +1,162 @@
+/*
+큐 활용하기
+
+간단한 알고리즘의 미팅 중개 프로그램을 작성한다.
+2개의 원형큐를 사용하며, 각각 남학생과 여학생의 대기 큐이다.
+새로운 남(여)학생이 등록을 하면 여(남)학생 큐에서 삭제하여 미팅이 성사된다. 
+만약 여(남)학생 큐에 이미 대기하고 있는 학생이 없으면 남(여)학생은 큐에 들어가서 기다리기로 한다.
+
+랜덤으로 발생되는 홀수와 짝수로 남녀 학생을 구분하기로 한다
+큐에는 학생의 이름이 입력된다.
+전체 100명의 학생(남녀 랜덤)에 대해서 미팅을 중개하며,
+미팅이 성사될 때마다, 화면에 내용을 출력한다.
+
+
+(예)
+남1 입장 - 대기
+남2 입장 - 대기
+남3 입장 - 대기
+여1 입장 - 여1/남1 매칭
+여2 입장 - 여2/남2 매칭
+남4 입장 - 대기
+남5 입장 - 대기
+여3 입장 - 여3/남3 매칭
+여4 입장 - 여4/남4  매칭
+여5 입장 - 여5/남5 매칭
+여6 입장 - 대기
+여7 입장 - 대기
+*/
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <windows.h>
+#define MAX_QUEUE_SIZE  20
+
+typedef struct {
+	char name[10];
+}element;
+
+typedef struct { // 큐 타입
+	element  data[MAX_QUEUE_SIZE];
+	int  front, rear;
+} QueueType;
+
+
+
+// 오류 함수
+
+void error(char* message){
+	fprintf(stderr, "%s\n", message);
+	exit(1);
+}
+
+
+
+// 공백 상태 검출 함수
+
+void init_queue(QueueType* q){
+	q->front = q->rear = 0;
+}
+
+
+
+// 공백 상태 검출 함수
+
+int is_empty(QueueType* q){
+	return (q->front == q->rear);
+}
+
+
+
+// 포화 상태 검출 함수
+
+int is_full(QueueType* q){
+	return ((q->rear + 1) % MAX_QUEUE_SIZE == q->front);
+}
+
+
+
+// 원형큐 출력 함
+void queue_print(QueueType* q){
+	printf("QUEUE(front=%d rear=%d) = ", q->front, q->rear);
+	if (!is_empty(q)) {
+		int i = q->front;
+		do {
+			i = (i + 1) % (MAX_QUEUE_SIZE);
+			printf("%d | ", q->data[i]);
+			if (i == q->rear)
+				break;
+
+		} while (i != q->front);
+	}
+	printf("\n");
+}
+
+
+
+
+// 삽입 함수
+
+void enqueue(QueueType* q, element item){
+	if (is_full(q))
+		error("큐가 포화상태입니다");
+	q->rear = (q->rear + 1) % MAX_QUEUE_SIZE;
+	q->data[q->rear] = item;
+}
+
+
+
+// 삭제 함수
+element dequeue(QueueType* q){
+	if (is_empty(q))
+		error("큐가 공백상태입니다");
+	q->front = (q->front + 1) % MAX_QUEUE_SIZE;
+	return q->data[q->front];
+}
+
+
+
+// 참조 함수
+element peek(QueueType* q){
+	if (is_empty(q))
+		error("큐가 공백상태입니다");
+
+	return q->data[(q->front + 1) % MAX_QUEUE_SIZE];
+}
+
+
+int main(void) {
+	QueueType man;
+	QueueType woman;
+	init_queue(&man);
+	init_queue(&woman);
+	int w = 0, m = 0;
+
+	for (int i = 0; i < 100; i++) {
+		element n;
+		int random = rand() % 2;
+
+		if (random == 1) { // 남자
+			sprintf(n.name, "남%d", ++m);
+			enqueue(&man, n);
+			if (!is_empty(&man) && !is_empty(&woman))
+				printf("남%d 입장 - %s / %s 매칭\n", m, dequeue(&woman).name, dequeue(&man).name);
+			
+			else
+				printf("남%d 입장 - 대기\n",m);
+		}
+
+		else if (random == 0) { // 여자
+			sprintf(n.name, "여%d", ++w);
+			enqueue(&woman, n);
+			if (!is_empty(&man) && !is_empty(&woman)) {
+				printf("여%d 입장 - %s / %s 매칭\n", w, dequeue(&woman).name, dequeue(&man).name);
+			}
+			else
+				printf("여%d 입장 - 대기\n", w);
+		}
+
+	}
+
+	return 0;
+}
